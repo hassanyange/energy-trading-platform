4f5bb26a9626680498fff9cd419c806d9d1ca142
benny-png <mazikuben2@gmail.com>
2024-07-11 19:00:02 +0300

    some changes

--- main/authentication.py
+++
@@ -1,6 +0,0 @@
-# your_app/authentication.py
-
-from rest_framework.authentication import TokenAuthentication
-
-class CustomTokenAuthentication(TokenAuthentication):
-    keyword = 'Token'  # Customize the keyword if needed
