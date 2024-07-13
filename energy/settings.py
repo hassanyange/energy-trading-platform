73df4328364b71fbd74cacdf8300c2d532e95a17
benny-png <mazikuben2@gmail.com>
2024-07-13 20:51:37 +0300

    some changes

--- energy/settings.py
+++ energy/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -50,7 +51,7 @@ INSTALLED_APPS = [
     'main',
     'rest_framework',
     'rest_framework_simplejwt',
-     'corsheaders',
+    'corsheaders',
 
 ]
 
