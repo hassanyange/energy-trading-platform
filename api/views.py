31ec3954c535239962a2d0ad1c1e622d0dea6a83
benny-png <mazikuben2@gmail.com>
2024-07-18 22:56:15 +0300

    some chnages

--- api/views.py
+++ api/views.py
@@ -114,9 +114,22 @@ class EnergyListView(generics.ListCreateAPIView):
 class TransactionListView(generics.ListCreateAPIView):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
-    
 
-  
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response({
+            'message': 'Transactions retrieved successfully',
+            'data': serializer.data
+        }, status=status.HTTP_200_OK)
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response({
+            'message': 'Transaction created successfully',
+            'data': serializer.data
+        }, status=status.HTTP_201_CREATED)
 
 
